229ce133220199cee41afb30bf24de253e655978
pedroalfonzoruvixx <pedroalfonzo@ruvixx.com>
2025-10-15 03:30:20 -0400

    Update utm-fill.js

--- utm-fill.js
+++ utm-fill.js
@@ -1,24 +1,24 @@
 function getReferrer() {
-  setTimeout(function () {
-    var queryString = window.location.search;
-    var urlParams = new URLSearchParams(queryString);
+  var queryString = window.location.search;
+  var urlParams = new URLSearchParams(queryString);
 
-    var utmSource = urlParams.get('utm_source');
-    var utmMedium = urlParams.get('utm_medium');
-    var utmCampaign = urlParams.get('utm_campaign');
+  var utmSource = urlParams.get('utm_source');
+  var utmMedium = urlParams.get('utm_medium');
+  var utmCampaign = urlParams.get('utm_campaign');
 
-    try {
-      loader.engine.document.getElementById(119209168).setValue({ value: utmSource });
-      loader.engine.document.getElementById(119209170).setValue({ value: utmMedium });
-      loader.engine.document.getElementById(119209171).setValue({ value: utmCampaign });
+  try {
+    loader.engine.document.getElementById(119209168).setValue({ value: utmSource });
+    loader.engine.document.getElementById(119209170).setValue({ value: utmMedium });
+    loader.engine.document.getElementById(119209171).setValue({ value: utmCampaign });
 
-      console.log('✅ UTM valores asignados correctamente');
-    } catch (e) {
-      console.error('❌ Error al asignar valores con loader.engine:', e);
-    }
-  }, 1000);
+    console.log('✅ UTM valores asignados correctamente');
+  } catch (e) {
+    console.error('❌ Error al asignar valores con loader.engine:', e);
+  }
 }
 
-window.onclick = getReferrer;
+window.addEventListener('load', getReferrer);
+
+
 
 
