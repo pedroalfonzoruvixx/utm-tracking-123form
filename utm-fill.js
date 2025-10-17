c3522d885aff4c876d1ea42126ecd6ba68374c0e
pedroalfonzoruvixx <pedroalfonzo@ruvixx.com>
2025-10-17 14:33:10 -0400

    Update utm-fill.js

--- utm-fill.js
+++ utm-fill.js
@@ -1,21 +1,32 @@
-function getReferrer() {
-  setTimeout(function () {
-    var queryString = window.location.search;
-    var urlParams = new URLSearchParams(queryString);
+jQuery(document).ready(function() {
+  setTimeout(function() {
+    var urlParams = new URLSearchParams(window.location.search);
 
-    // Corrección de nombres: source, medium, campaign
-    var utmSource = urlParams.get('utm_source');
-    var utmMedium = urlParams.get('utm_medium');
-    var utmCampaign = urlParams.get('utm_campaign');
+    var utmSource = urlParams.get('utm_source') || '';
+    var utmMedium = urlParams.get('utm_medium') || '';
+    var utmCampaign = urlParams.get('utm_campaign') || '';
+    var utmId = urlParams.get('utm_id') || '';
 
-    // IDs reales de los campos ocultos en 123FormBuilder
-    loader.engine.document.getElementById(119209168).setValue({ value: utmSource });
-    loader.engine.document.getElementById(119209170).setValue({ value: utmMedium });
-    loader.engine.document.getElementById(119209171).setValue({ value: utmCampaign });
-  }, 1000);
-}
+    // Combinamos todos los UTM en un solo string
+    var combinedUTM = `source=${utmSource}&medium=${utmMedium}&campaign=${utmCampaign}&id=${utmId}`;
+
+    // ID del campo oculto donde querés guardar el string combinado
+    var targetControl = loader.getEngine().getDocument().getControlsList().find(function(control) {
+      return control.id === 119209171; // ← reemplazá con el ID del campo oculto que usarás
+    });
+
+    if (targetControl) {
+      targetControl.setValue({ value: combinedUTM });
+      console.log('UTM combinados asignados:', combinedUTM);
+    } else {
+      console.warn('No se encontró el control con ID 119209171');
+    }
+  }, 2000);
+});
+
 
 window.onclick = getReferrer;
+
 
 
 
